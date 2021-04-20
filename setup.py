b8d6169e0654b1b7ef233f8573246af17c181e99
Arina Maltseva <arina@Arinas-MacBook-Pro.local>
2021-04-19 21:16:44 -0700

    Added long_description_content_type field

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     author_email='arina@vectorspace.ai',
     description='Google Drive API for service accounts',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     install_requires=[
         "google-api-python-client>=2.1.0",
         "google-auth-httplib2>=0.1.0",
