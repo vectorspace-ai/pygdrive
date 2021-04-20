6958da25b559c2438cd74e777821c39644bebd24
Arina Maltseva <arina@Arinas-MacBook-Pro.local>
2021-04-19 19:59:19 -0700

    Implementing Test finished

--- pygdrive/tests/test_drive.py
+++ pygdrive/tests/test_drive.py
@@ -1,45 +1,77 @@
 import unittest
+import pygdrive
+
 import unittest.mock as mock
-from pygdrive import Gdrive
-from pygdrive.tests.test_util import *
 
-TEST_ATTR = read_settings_file()
-
-CREDENTIALS_FILEPATH = TEST_ATTR['test_config_file']
-SCOPES = TEST_ATTR['scope']
-DOWNLOAD_DIR = TEST_ATTR['download_dir']
-UPLOAD_DIR = TEST_ATTR['upload_dir']
+CREDENTIALS_FILEPATH = {}
+SCOPES = []
+DOWNLOAD_DIR = ''
+UPLOAD_DIR = ''
 
 
-class TestDrive(unittest.TestCase):
+class TestBasic(unittest.TestCase):
+    """
+    Base class
+    """
+    @mock.patch.object(pygdrive.gdrive, 'build', autospec=True)
+    @mock.patch.object(pygdrive.gdrive.service_account.Credentials, 'from_service_account_file', autospec=True)
+    def setUp(self, mock_credential_object, mock_build_object):
+        self.test_object = pygdrive.gdrive.Gdrive(CREDENTIALS_FILEPATH, SCOPES, DOWNLOAD_DIR, UPLOAD_DIR)
 
+    def tearDown(self):
+        self.test_object = None
+
+
+class TestDrive(TestBasic):
     def test_object_create(self):
-        test_object = Gdrive(CREDENTIALS_FILEPATH, SCOPES, DOWNLOAD_DIR, UPLOAD_DIR)
-        self.assertTrue(test_object is not None)
+        self.assertTrue(self.test_object is not None)
 
 
-class TestUploading(unittest.TestCase):
-    gd = Gdrive(CREDENTIALS_FILEPATH, SCOPES, DOWNLOAD_DIR, UPLOAD_DIR)
+class TestFolder(TestBasic):
+    def test_find_folder(self):
+        mock_id, mock_name = 'test_id123', 'test_name'
+        self.test_object.service.files.return_value.list.return_value.execute.return_value = {
+            'files': [{'id': mock_id, 'name': mock_name}]}
+        id, name = self.test_object.find_folder('test_file')
+        self.assertEqual(mock_id, id)
 
-    def test_object_upload(self):
-        ...
+    def test_folder_create(self):
+        mock_parent_id = 'parent_id123'
+        mock_folder_id, mock_folder_name = 'test_id123', 'folder_test'
+        self.test_object.service.files.return_value.create.return_value.execute.return_value = {'id': mock_folder_id}
+        folder_id = self.test_object.create_folder(mock_parent_id, mock_folder_name)
+        self.assertEqual(folder_id, mock_folder_id)
 
 
-class TestDownloading(unittest.TestCase):
-    gd = Gdrive(CREDENTIALS_FILEPATH, SCOPES, DOWNLOAD_DIR, UPLOAD_DIR)
+class TestUploading(TestBasic):
+    @mock.patch.object(pygdrive.gdrive, 'MediaFileUpload', autospec=True)
+    def test_object_upload(self, mock_media_upload):
+        mock_parent_id = 'parent_id123'
+        mock_file_id, mock_file_name = 'test_id123', 'file_test2.csv'
+        self.test_object.service.files.return_value.create.return_value.execute.return_value = {'id': mock_file_id}
+        file_id = self.test_object.upload_file(mock_parent_id, mock_file_name)
+        self.assertEqual(file_id, mock_file_id)
 
-    def test_object_download(self):
-        ...
+
+# class TestDownloading(TestBasic):
+#     def test_object_download(self):
+#         mock_file_id, mock_file_name = 'test_id123', 'file_test2.csv'
+#         self.test_object.service.files.return_value.get_media.return_value.execute.return_value = None
+#         return_value = self.test_object.download_file(mock_file_id, mock_file_name)
+#         self.assertEqual(None, return_value)
 
 
-class TestFileSearching(unittest.TestCase):
-    gd = Gdrive(CREDENTIALS_FILEPATH, SCOPES, DOWNLOAD_DIR, UPLOAD_DIR)
-
+class TestFileSearching(TestBasic):
     def test_search_existing_file(self):
-        ...
-    def test_search_random_file(self):
-        ...
+        mock_file_id, mock_file_name = 'test_id123', 'file_test2.csv'
+        self.test_object.service.files.return_value.list.return_value.execute.return_value = {
+            'files': [{'id': mock_file_id, 'name': mock_file_name}]}
+        id, name = self.test_object.find_file(mock_file_name)
+        self.assertEqual(id, mock_file_id)
+
     def test_search_existing_folder(self):
-        ...
-    def test_search_random_folder(self):
-        ...
+        mock_folder_id, mock_folder_name = 'test_id123', 'file_test2.csv'
+        self.test_object.service.files.return_value.list.return_value.execute.return_value = {
+            'files': [{'id': mock_folder_id, 'name': mock_folder_name}]}
+        id, name = self.test_object.find_folder(mock_folder_name)
+        self.assertEqual(id, mock_folder_id)
--- pygdrive/tests/test_util.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-SETTINGS_FILEPATH = '../tests/settings/default.yaml'
-
-def read_settings_file():
-    with open(SETTINGS_FILEPATH) as file:
-        TEST_ATTR = yaml.load(file, Loader=yaml.FullLoader)
-    return TEST_ATTR
